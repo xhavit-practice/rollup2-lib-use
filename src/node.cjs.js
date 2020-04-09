b26a81e26e78ea16b4adfd4a89c4df90dd1b22a4
xhavit <xhavit@qq.com>
2020-04-09 21:16:48 +0800

    feat: init

---
+++ src/node.cjs.js
@@ -0,0 +1,5 @@
+const rollup2Lib = require('rollup2-lib');
+
+rollup2Lib.method1();
+rollup2Lib.method2();
+rollup2Lib.method3();
---
+++ src/webpack.cjs.js
@@ -0,0 +1,5 @@
+import rollup2Lib from 'rollup2-lib/dist/rollup2-lib.cjs';
+
+rollup2Lib.method1();
+rollup2Lib.method2();
+rollup2Lib.method3();
